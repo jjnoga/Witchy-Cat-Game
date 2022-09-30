b4a4086a5b6e745a5827c354a04ad5cf02fe3b52
Michael Ruocco <michael.ruocco@quinnipiac.edu>
2022-09-30 15:45:00 -0400

    added selection to textboxes

--- src/Level/Player.java
+++ src/Level/Player.java
@@ -40,152 +40,176 @@ public abstract class Player extends GameObject {
     protected Key MOVE_UP_KEY = Key.UP;
     protected Key MOVE_DOWN_KEY = Key.DOWN;
     protected Key INTERACT_KEY = Key.SPACE;
+    protected Key SPRINT_KEY = Key.SHIFT;
 
     public Player(SpriteSheet spriteSheet, float x, float y, String startingAnimationName) {
-        super(spriteSheet, x, y, startingAnimationName);
-        facingDirection = Direction.RIGHT;
-        playerState = PlayerState.STANDING;
-        previousPlayerState = playerState;
-        this.affectedByTriggers = true;
+	super(spriteSheet, x, y, startingAnimationName);
+	facingDirection = Direction.RIGHT;
+	playerState = PlayerState.STANDING;
+	previousPlayerState = playerState;
+	this.affectedByTriggers = true;
     }
 
     public void update() {
-        moveAmountX = 0;
-        moveAmountY = 0;
+	moveAmountX = 0;
+	moveAmountY = 0;
 
-        // if player is currently playing through level (has not won or lost)
-        // update player's state and current actions, which includes things like determining how much it should move each frame and if its walking or jumping
-        do {
-            previousPlayerState = playerState;
-            handlePlayerState();
-        } while (previousPlayerState != playerState);
+	// if player is currently playing through level (has not won or lost)
+	// update player's state and current actions, which includes things like
+	// determining how much it should move each frame and if its walking or jumping
+	do {
+	    previousPlayerState = playerState;
+	    handlePlayerState();
+	} while (previousPlayerState != playerState);
 
-        // move player with respect to map collisions based on how much player needs to move this frame
-        if (playerState != PlayerState.INTERACTING) {
-            lastAmountMovedY = super.moveYHandleCollision(moveAmountY);
-            lastAmountMovedX = super.moveXHandleCollision(moveAmountX);
-        }
+	// move player with respect to map collisions based on how much player needs to
+	// move this frame
+	if (playerState != PlayerState.INTERACTING) {
+	    lastAmountMovedY = super.moveYHandleCollision(moveAmountY);
+	    lastAmountMovedX = super.moveXHandleCollision(moveAmountX);
+	}
 
-        handlePlayerAnimation();
+	handlePlayerAnimation();
 
-        updateLockedKeys();
+	updateLockedKeys();
 
-        // update player's animation
-        super.update();
+	// update player's animation
+	super.update();
     }
 
-    // based on player's current state, call appropriate player state handling method
+    // based on player's current state, call appropriate player state handling
+    // method
     protected void handlePlayerState() {
-        switch (playerState) {
-            case STANDING:
-                playerStanding();
-                break;
-            case WALKING:
-                playerWalking();
-                break;
-            case INTERACTING:
-                playerInteracting();
-                break;
-        }
+	switch (playerState) {
+	case STANDING:
+	    playerStanding();
+	    break;
+	case WALKING:
+	    playerWalking();
+	    break;
+	case INTERACTING:
+	    playerInteracting();
+	    break;
+	}
     }
 
     // player STANDING state logic
     protected void playerStanding() {
-        if (!keyLocker.isKeyLocked(INTERACT_KEY) && Keyboard.isKeyDown(INTERACT_KEY)) {
-            keyLocker.lockKey(INTERACT_KEY);
-            map.entityInteract(this);
-        }
+	if (!keyLocker.isKeyLocked(INTERACT_KEY) && Keyboard.isKeyDown(INTERACT_KEY)) {
+	    keyLocker.lockKey(INTERACT_KEY);
+	    map.entityInteract(this);
+	}
 
-        // if a walk key is pressed, player enters WALKING state
-        if (Keyboard.isKeyDown(MOVE_LEFT_KEY) || Keyboard.isKeyDown(MOVE_RIGHT_KEY) || Keyboard.isKeyDown(MOVE_UP_KEY) || Keyboard.isKeyDown(MOVE_DOWN_KEY)) {
-            playerState = PlayerState.WALKING;
-        }
+	// if a walk key is pressed, player enters WALKING state
+	if (Keyboard.isKeyDown(MOVE_LEFT_KEY) || Keyboard.isKeyDown(MOVE_RIGHT_KEY) || Keyboard.isKeyDown(MOVE_UP_KEY)
+		|| Keyboard.isKeyDown(MOVE_DOWN_KEY)) {
+	    playerState = PlayerState.WALKING;
+	}
     }
 
     // player WALKING state logic
     protected void playerWalking() {
-        if (!keyLocker.isKeyLocked(INTERACT_KEY) && Keyboard.isKeyDown(INTERACT_KEY)) {
-            keyLocker.lockKey(INTERACT_KEY);
-            map.entityInteract(this);
-        }
+	if (!keyLocker.isKeyLocked(INTERACT_KEY) && Keyboard.isKeyDown(INTERACT_KEY)) {
+	    keyLocker.lockKey(INTERACT_KEY);
+	    map.entityInteract(this);
+	}
 
-        // if walk left key is pressed, move player to the left
-        if (Keyboard.isKeyDown(MOVE_LEFT_KEY)) {
-            moveAmountX -= walkSpeed;
-            facingDirection = Direction.LEFT;
-            currentWalkingXDirection = Direction.LEFT;
-            lastWalkingXDirection = Direction.LEFT;
-        }
+	// if walk left key is pressed, move player to the left
+	if (Keyboard.isKeyDown(MOVE_LEFT_KEY)) {
+	    if (Keyboard.isKeyDown(SPRINT_KEY)) {
+		moveAmountX -= walkSpeed + 3;
+	    } else {
+		moveAmountX -= walkSpeed;
+	    }
+	    facingDirection = Direction.LEFT;
+	    currentWalkingXDirection = Direction.LEFT;
+	    lastWalkingXDirection = Direction.LEFT;
+	}
 
-        // if walk right key is pressed, move player to the right
-        else if (Keyboard.isKeyDown(MOVE_RIGHT_KEY)) {
-            moveAmountX += walkSpeed;
-            facingDirection = Direction.RIGHT;
-            currentWalkingXDirection = Direction.RIGHT;
-            lastWalkingXDirection = Direction.RIGHT;
-        }
-        else {
-            currentWalkingXDirection = Direction.NONE;
-        }
+	// if walk right key is pressed, move player to the right
+	else if (Keyboard.isKeyDown(MOVE_RIGHT_KEY)) {
+	    if (Keyboard.isKeyDown(SPRINT_KEY)) {
+		moveAmountX += walkSpeed + 3;
+	    } else {
+		moveAmountX += walkSpeed;
+	    }
+	    facingDirection = Direction.RIGHT;
+	    currentWalkingXDirection = Direction.RIGHT;
+	    lastWalkingXDirection = Direction.RIGHT;
+	} else {
+	    currentWalkingXDirection = Direction.NONE;
+	}
 
-        if (Keyboard.isKeyDown(MOVE_UP_KEY)) {
-            moveAmountY -= walkSpeed;
-            currentWalkingYDirection = Direction.UP;
-            lastWalkingYDirection = Direction.UP;
-        }
-        else if (Keyboard.isKeyDown(MOVE_DOWN_KEY)) {
-            moveAmountY += walkSpeed;
-            currentWalkingYDirection = Direction.DOWN;
-            lastWalkingYDirection = Direction.DOWN;
-        }
-        else {
-            currentWalkingYDirection = Direction.NONE;
-        }
+	if (Keyboard.isKeyDown(MOVE_UP_KEY)) {
+	    if (Keyboard.isKeyDown(SPRINT_KEY)) {
+		moveAmountY -= walkSpeed + 3;
+	    } else {
+		moveAmountY -= walkSpeed;
+	    }
+	    currentWalkingYDirection = Direction.UP;
+	    lastWalkingYDirection = Direction.UP;
+	} else if (Keyboard.isKeyDown(MOVE_DOWN_KEY)) {
+	    if (Keyboard.isKeyDown(SPRINT_KEY)) {
+		moveAmountY += walkSpeed + 3;
+	    } else {
+		moveAmountY += walkSpeed;
+	    }
+	    currentWalkingYDirection = Direction.DOWN;
+	    lastWalkingYDirection = Direction.DOWN;
+	} else {
+	    currentWalkingYDirection = Direction.NONE;
+	}
 
-        if ((currentWalkingXDirection == Direction.RIGHT || currentWalkingXDirection == Direction.LEFT) && currentWalkingYDirection == Direction.NONE) {
-            lastWalkingYDirection = Direction.NONE;
-        }
+	if ((currentWalkingXDirection == Direction.RIGHT || currentWalkingXDirection == Direction.LEFT)
+		&& currentWalkingYDirection == Direction.NONE) {
+	    lastWalkingYDirection = Direction.NONE;
+	}
 
-        if ((currentWalkingYDirection == Direction.UP || currentWalkingYDirection == Direction.DOWN) && currentWalkingXDirection == Direction.NONE) {
-            lastWalkingXDirection = Direction.NONE;
-        }
+	if ((currentWalkingYDirection == Direction.UP || currentWalkingYDirection == Direction.DOWN)
+		&& currentWalkingXDirection == Direction.NONE) {
+	    lastWalkingXDirection = Direction.NONE;
+	}
 
-        if (Keyboard.isKeyUp(MOVE_LEFT_KEY) && Keyboard.isKeyUp(MOVE_RIGHT_KEY) && Keyboard.isKeyUp(MOVE_UP_KEY) && Keyboard.isKeyUp(MOVE_DOWN_KEY)) {
-            playerState = PlayerState.STANDING;
-        }
+	if (Keyboard.isKeyUp(MOVE_LEFT_KEY) && Keyboard.isKeyUp(MOVE_RIGHT_KEY) && Keyboard.isKeyUp(MOVE_UP_KEY)
+		&& Keyboard.isKeyUp(MOVE_DOWN_KEY)) {
+	    playerState = PlayerState.STANDING;
+	}
     }
 
-    // player INTERACTING state logic -- intentionally does nothing so player is locked in place while a script is running
-    protected void playerInteracting() { }
+    // player INTERACTING state logic -- intentionally does nothing so player is
+    // locked in place while a script is running
+    protected void playerInteracting() {
+    }
 
     protected void updateLockedKeys() {
-        if (Keyboard.isKeyUp(INTERACT_KEY) && playerState != PlayerState.INTERACTING) {
-            keyLocker.unlockKey(INTERACT_KEY);
-        }
+	if (Keyboard.isKeyUp(INTERACT_KEY) && playerState != PlayerState.INTERACTING) {
+	    keyLocker.unlockKey(INTERACT_KEY);
+	}
     }
 
     // anything extra the player should do based on interactions can be handled here
     protected void handlePlayerAnimation() {
-        if (playerState == PlayerState.STANDING) {
-            // sets animation to a STAND animation based on which way player is facing
-            this.currentAnimationName = facingDirection == Direction.RIGHT ? "STAND_RIGHT" : "STAND_LEFT";
-        }
-        else if (playerState == PlayerState.WALKING) {
-            // sets animation to a WALK animation based on which way player is facing
-            this.currentAnimationName = facingDirection == Direction.RIGHT ? "WALK_RIGHT" : "WALK_LEFT";
-        }
-        else if (playerState == PlayerState.INTERACTING) {
-            // sets animation to STAND when player is interacting
-            // player can be told to stand or walk during Script by using the "stand" and "walk" methods
-            this.currentAnimationName = facingDirection == Direction.RIGHT ? "STAND_RIGHT" : "STAND_LEFT";
-        }
+	if (playerState == PlayerState.STANDING) {
+	    // sets animation to a STAND animation based on which way player is facing
+	    this.currentAnimationName = facingDirection == Direction.RIGHT ? "STAND_RIGHT" : "STAND_LEFT";
+	} else if (playerState == PlayerState.WALKING) {
+	    // sets animation to a WALK animation based on which way player is facing
+	    this.currentAnimationName = facingDirection == Direction.RIGHT ? "WALK_RIGHT" : "WALK_LEFT";
+	} else if (playerState == PlayerState.INTERACTING) {
+	    // sets animation to STAND when player is interacting
+	    // player can be told to stand or walk during Script by using the "stand" and
+	    // "walk" methods
+	    this.currentAnimationName = facingDirection == Direction.RIGHT ? "STAND_RIGHT" : "STAND_LEFT";
+	}
     }
 
     @Override
-    public void onEndCollisionCheckX(boolean hasCollided, Direction direction, MapEntity entityCollidedWith) { }
+    public void onEndCollisionCheckX(boolean hasCollided, Direction direction, MapEntity entityCollidedWith) {
+    }
 
     @Override
-    public void onEndCollisionCheckY(boolean hasCollided, Direction direction, MapEntity entityCollidedWith) { }
+    public void onEndCollisionCheckY(boolean hasCollided, Direction direction, MapEntity entityCollidedWith) {
+    }
 
     // other entities can call this method to hurt the player
     public void hurtPlayer(MapEntity mapEntity) {
@@ -193,76 +217,80 @@ public abstract class Player extends GameObject {
     }
 
     public PlayerState getPlayerState() {
-        return playerState;
+	return playerState;
     }
 
     public void setPlayerState(PlayerState playerState) {
-        this.playerState = playerState;
+	this.playerState = playerState;
     }
 
     public Direction getFacingDirection() {
-        return facingDirection;
+	return facingDirection;
     }
 
     public void setFacingDirection(Direction facingDirection) {
-        this.facingDirection = facingDirection;
+	this.facingDirection = facingDirection;
     }
 
     public void addListener(PlayerListener listener) {
-        listeners.add(listener);
+	listeners.add(listener);
     }
 
     public Rectangle getInteractionRange() {
-        return new Rectangle(
-                getBounds().getX1() - interactionRange,
-                getBounds().getY1() - interactionRange,
-                getBounds().getWidth() + (interactionRange * 2),
-                getBounds().getHeight() + (interactionRange * 2));
+	return new Rectangle(getBounds().getX1() - interactionRange, getBounds().getY1() - interactionRange,
+		getBounds().getWidth() + (interactionRange * 2), getBounds().getHeight() + (interactionRange * 2));
     }
 
-    public Key getInteractKey() { return INTERACT_KEY; }
-    public Direction getCurrentWalkingXDirection() { return currentWalkingXDirection; }
-    public Direction getCurrentWalkingYDirection() { return currentWalkingYDirection; }
-    public Direction getLastWalkingXDirection() { return lastWalkingXDirection; }
-    public Direction getLastWalkingYDirection() { return lastWalkingYDirection; }
+    public Key getInteractKey() {
+	return INTERACT_KEY;
+    }
+
+    public Direction getCurrentWalkingXDirection() {
+	return currentWalkingXDirection;
+    }
+
+    public Direction getCurrentWalkingYDirection() {
+	return currentWalkingYDirection;
+    }
+
+    public Direction getLastWalkingXDirection() {
+	return lastWalkingXDirection;
+    }
+
+    public Direction getLastWalkingYDirection() {
+	return lastWalkingYDirection;
+    }
 
     public void stand(Direction direction) {
-        facingDirection = direction;
-        if (direction == Direction.RIGHT) {
-            this.currentAnimationName = "STAND_RIGHT";
-        }
-        else if (direction == Direction.LEFT) {
-            this.currentAnimationName = "STAND_LEFT";
-        }
+	facingDirection = direction;
+	if (direction == Direction.RIGHT) {
+	    this.currentAnimationName = "STAND_RIGHT";
+	} else if (direction == Direction.LEFT) {
+	    this.currentAnimationName = "STAND_LEFT";
+	}
     }
 
     public void walk(Direction direction, float speed) {
-        facingDirection = direction;
-        if (direction == Direction.RIGHT) {
-            this.currentAnimationName = "WALK_RIGHT";
-        }
-        else if (direction == Direction.LEFT) {
-            this.currentAnimationName = "WALK_LEFT";
-        }
-        else {
-            if (this.currentAnimationName.contains("RIGHT")) {
-                this.currentAnimationName = "WALK_RIGHT";
-            }
-            else {
-                this.currentAnimationName = "WALK_LEFT";
-            }
-        }
-        if (direction == Direction.UP) {
-            moveY(-speed);
-        }
-        else if (direction == Direction.DOWN) {
-            moveY(speed);
-        }
-        else if (direction == Direction.LEFT) {
-            moveX(-speed);
-        }
-        else if (direction == Direction.RIGHT) {
-            moveX(speed);
-        }
+	facingDirection = direction;
+	if (direction == Direction.RIGHT) {
+	    this.currentAnimationName = "WALK_RIGHT";
+	} else if (direction == Direction.LEFT) {
+	    this.currentAnimationName = "WALK_LEFT";
+	} else {
+	    if (this.currentAnimationName.contains("RIGHT")) {
+		this.currentAnimationName = "WALK_RIGHT";
+	    } else {
+		this.currentAnimationName = "WALK_LEFT";
+	    }
+	}
+	if (direction == Direction.UP) {
+	    moveY(-speed);
+	} else if (direction == Direction.DOWN) {
+	    moveY(speed);
+	} else if (direction == Direction.LEFT) {
+	    moveX(-speed);
+	} else if (direction == Direction.RIGHT) {
+	    moveX(speed);
+	}
     }
 }
