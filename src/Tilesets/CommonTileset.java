76d964c8d1bc9acec20a1c59a0181929dd3d8239
Michael Ruocco <michael.ruocco@quinnipiac.edu>
2022-10-02 11:26:06 -0400

    changed tileSet loaded

--- src/Tilesets/CommonTileset.java
+++ src/Tilesets/CommonTileset.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 public class CommonTileset extends Tileset {
 
     public CommonTileset() {
-	super(ImageLoader.load("CommonTileset2.png"), 16, 16, 3);
+	super(ImageLoader.load("CommonTileset.png"), 16, 16, 3);
     }
 
     @Override
