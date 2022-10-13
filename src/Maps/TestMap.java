7f68676d7b5f24b8137e1bda7cd5b7607042b6f0
madi-smith <mesmith10@quinnipiac.edu>
2022-10-13 13:28:07 -0400

    fixed the rocks

--- src/Maps/TestMap.java
+++ src/Maps/TestMap.java
@@ -53,16 +53,6 @@ public class TestMap extends Map {
 		enhancedMapTiles.add(new Coin(getMapTile(55, 24).getLocation(), this));
 		enhancedMapTiles.add(new Coin(getMapTile(69, 2).getLocation(), this));
 		
-		enhancedMapTiles.add(new Rock(getMapTile(55, 19).getLocation()));
-		enhancedMapTiles.add(new Rock(getMapTile(54, 19).getLocation()));
-		enhancedMapTiles.add(new Rock(getMapTile(77, 4).getLocation()));
-		enhancedMapTiles.add(new Rock(getMapTile(77, 5).getLocation()));
-		enhancedMapTiles.add(new Rock(getMapTile(72, 4).getLocation()));
-		enhancedMapTiles.add(new Rock(getMapTile(73, 4).getLocation()));
-		enhancedMapTiles.add(new Rock(getMapTile(64, 4).getLocation()));
-		enhancedMapTiles.add(new Rock(getMapTile(65, 4).getLocation()));
-		enhancedMapTiles.add(new Coin(getMapTile(55, 24).getLocation(), this));
-		enhancedMapTiles.add(new Coin(getMapTile(69, 2).getLocation(), this));
 		return enhancedMapTiles;
 	}
 
