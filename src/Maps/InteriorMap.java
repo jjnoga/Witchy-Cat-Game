a04304b9039edc0e0ef293ec3eb6e5a6f462ae6e
Michael Ruocco <michael.ruocco@quinnipiac.edu>
2022-10-21 11:44:08 -0400

    added interior to walrus house

---
+++ src/Maps/InteriorMap.java
@@ -0,0 +1,40 @@
+package Maps;
+
+import Level.EnhancedMapTile;
+
+import Level.Map;
+import Level.NPC;
+import Level.Trigger;
+import Tilesets.CommonTileset;
+
+import java.util.ArrayList;
+
+// Represents a test map to be used in a level
+public class InteriorMap extends Map {
+
+	public InteriorMap() {
+		super("interior_map.txt", new CommonTileset());		
+	}
+
+	@Override
+	public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
+		ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();		
+		return enhancedMapTiles;
+	}
+
+	@Override
+	public ArrayList<NPC> loadNPCs() {
+		ArrayList<NPC> npcs = new ArrayList<>();
+		return npcs;
+	}
+
+	@Override
+	public ArrayList<Trigger> loadTriggers() {
+		ArrayList<Trigger> triggers = new ArrayList<>();		
+		return triggers;
+	}
+
+	@Override
+	public void loadScripts() {	
+	}
+}
--- src/Screens/PlayLevelScreen.java
+++ src/Screens/PlayLevelScreen.java
@@ -17,7 +17,11 @@ import Game.GameState;
 import Game.ScreenCoordinator;
 
 import Level.*;
+import Maps.SummerMap;
 import Maps.TestMap;
+import Maps.WinterMap;
+import Maps.FallMap;
+import Maps.InteriorMap;
 import Players.Cat;
 import Scripts.Sounds;
 import Utils.Direction;
@@ -27,6 +31,11 @@ import Utils.Point;
 public class PlayLevelScreen extends Screen {
     protected ScreenCoordinator screenCoordinator;
     protected Map map;
+    protected Map springMap;
+    protected Map summerMap;
+    protected Map winterMap;
+    protected Map fallMap;
+    protected Map interiorMap;
     protected Player player;
     protected PlayLevelScreenState playLevelScreenState;
     protected WinScreen winScreen;
@@ -34,181 +43,205 @@ public class PlayLevelScreen extends Screen {
     protected KeyLocker keyLocker = new KeyLocker();
     Sounds sound = new Sounds();
 
-
     public PlayLevelScreen(ScreenCoordinator screenCoordinator) {
-        this.screenCoordinator = screenCoordinator;
-        
-       
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(1,3));
-        
-        JButton volUp = new JButton("Volume Up");
-        volUp.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				sound.volumeUp();
-			}
-		});
-        frame.add(volUp);
-        
-        JButton volDown = new JButton("Volume Down");
-        volDown.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				sound.volumeDown();
-			}
-		});
-        frame.add(volDown);
-        
-        JButton mute = new JButton("Mute/Unmute");
-        mute.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				sound.volumeMute();
-			}
-		});
-        frame.add(mute);
-        
-        frame.pack();
-        frame.setVisible(true);
-        
-        playMusic(0);
+	this.screenCoordinator = screenCoordinator;
+
+	JFrame frame = new JFrame();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setLayout(new GridLayout(1, 3));
+
+	JButton volUp = new JButton("Volume Up");
+	volUp.addActionListener(new ActionListener() {
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		sound.volumeUp();
+	    }
+	});
+	frame.add(volUp);
+
+	JButton volDown = new JButton("Volume Down");
+	volDown.addActionListener(new ActionListener() {
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		sound.volumeDown();
+	    }
+	});
+	frame.add(volDown);
+
+	JButton mute = new JButton("Mute/Unmute");
+	mute.addActionListener(new ActionListener() {
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		sound.volumeMute();
+	    }
+	});
+	frame.add(mute);
+
+	frame.pack();
+	frame.setVisible(true);
+
+	playMusic(0);
     }
 
     public void initialize() {
-        // setup state
-        flagManager = new FlagManager();
-        flagManager.addFlag("rightSpot", false);
-        flagManager.addFlag("hasLostBall", false);
-        flagManager.addFlag("hasTalkedToWalrus", false);
-        flagManager.addFlag("hasTalkedToDinosaur", false);
-        flagManager.addFlag("hasFoundBall", false);
-        flagManager.addFlag("hasFoundCoin", false);
-        flagManager.addFlag("hasTalkedToBlorbo", false);
-        flagManager.addFlag("hasTalkedtoPizza", false);
+	// setup state
+	flagManager = new FlagManager();
+	flagManager.addFlag("rightSpot", false);
+	flagManager.addFlag("hasLostBall", false);
+	flagManager.addFlag("hasTalkedToWalrus", false);
+	flagManager.addFlag("hasTalkedToDinosaur", false);
+	flagManager.addFlag("hasFoundBall", false);
+	flagManager.addFlag("hasFoundCoin", false);
+	flagManager.addFlag("hasTalkedToBlorbo", false);
+	flagManager.addFlag("hasTalkedtoPizza", false);
 
-        // define/setup map
-        this.map = new TestMap();
-        map.reset();
-        map.setFlagManager(flagManager);
+	// define/setup map
+	springMap = new TestMap();
+	summerMap = new SummerMap();
+	winterMap = new WinterMap();
+	fallMap = new FallMap();
+	interiorMap = new InteriorMap();
+	this.map = springMap;
+	map.reset();
+	map.setFlagManager(flagManager);
 
-        // setup player
-        this.player = new Cat(map.getPlayerStartPosition().x, map.getPlayerStartPosition().y);
-        this.player.setMap(map);
-        Point playerStartPosition = map.getPlayerStartPosition();
-        this.player.setLocation(playerStartPosition.x, playerStartPosition.y);
-        this.playLevelScreenState = PlayLevelScreenState.RUNNING;
-        this.player.setFacingDirection(Direction.LEFT);
-        
-        
-       
+	// setup player
+	this.player = new Cat(map.getPlayerStartPosition().x, map.getPlayerStartPosition().y);
+	this.player.setMap(map);
+	Point playerStartPosition = map.getPlayerStartPosition();
+	this.player.setLocation(playerStartPosition.x, playerStartPosition.y);
+	this.playLevelScreenState = PlayLevelScreenState.RUNNING;
+	this.player.setFacingDirection(Direction.LEFT);
 
-        // let pieces of map know which button to listen for as the "interact" button
-        map.getTextbox().setInteractKey(player.getInteractKey());
+	// let pieces of map know which button to listen for as the "interact" button
+	map.getTextbox().setInteractKey(player.getInteractKey());
 
-        // setup map scripts to have references to the map and player
-        for (MapTile mapTile : map.getMapTiles()) {
-            if (mapTile.getInteractScript() != null) {
-                mapTile.getInteractScript().setMap(map);
-                mapTile.getInteractScript().setPlayer(player);
-            }
-        }
-        for (NPC npc : map.getNPCs()) {
-            if (npc.getInteractScript() != null) {
-                npc.getInteractScript().setMap(map);
-                npc.getInteractScript().setPlayer(player);
-            }
-        }
-        for (EnhancedMapTile enhancedMapTile : map.getEnhancedMapTiles()) {
-            if (enhancedMapTile.getInteractScript() != null) {
-                enhancedMapTile.getInteractScript().setMap(map);
-                enhancedMapTile.getInteractScript().setPlayer(player);
-            }
-        }
-        for (Trigger trigger : map.getTriggers()) {
-            if (trigger.getTriggerScript() != null) {
-                trigger.getTriggerScript().setMap(map);
-                trigger.getTriggerScript().setPlayer(player);
-            }
-        }
+	// setup map scripts to have references to the map and player
+	for (MapTile mapTile : map.getMapTiles()) {
+	    if (mapTile.getInteractScript() != null) {
+		mapTile.getInteractScript().setMap(map);
+		mapTile.getInteractScript().setPlayer(player);
+	    }
+	}
+	for (NPC npc : map.getNPCs()) {
+	    if (npc.getInteractScript() != null) {
+		npc.getInteractScript().setMap(map);
+		npc.getInteractScript().setPlayer(player);
+	    }
+	}
+	for (EnhancedMapTile enhancedMapTile : map.getEnhancedMapTiles()) {
+	    if (enhancedMapTile.getInteractScript() != null) {
+		enhancedMapTile.getInteractScript().setMap(map);
+		enhancedMapTile.getInteractScript().setPlayer(player);
+	    }
+	}
+	for (Trigger trigger : map.getTriggers()) {
+	    if (trigger.getTriggerScript() != null) {
+		trigger.getTriggerScript().setMap(map);
+		trigger.getTriggerScript().setPlayer(player);
+	    }
+	}
 
-        winScreen = new WinScreen(this);
-        
-        
-        	
+	winScreen = new WinScreen(this);
+
     }
 
     public void update() {
-        // based on screen state, perform specific actions
-        switch (playLevelScreenState) {
-            // if level is "running" update player and map to keep game logic for the platformer level going
-            case RUNNING:
-                player.update();
-                map.update(player);
-                break;
-            // if level has been completed, bring up level cleared screen
-            case LEVEL_COMPLETED:
-                winScreen.update();
-                break;
-        }
+	// based on screen state, perform specific actions
+	switch (playLevelScreenState) {
+	// if level is "running" update player and map to keep game logic for the
+	// platformer level going
+	case RUNNING:
+	    player.update();
+	    map.update(player);
+	    break;
+	// if level has been completed, bring up level cleared screen
+	case LEVEL_COMPLETED:
+	    winScreen.update();
+	    break;
+	}
 
-        // if flag is set at any point during gameplay, game is "won"
-        if (map.getFlagManager().isFlagSet("hasFoundBall")) {
-            playLevelScreenState = PlayLevelScreenState.LEVEL_COMPLETED;
-        }
-        
+	// if flag is set at any point during gameplay, game is "won"
+	if (map.getFlagManager().isFlagSet("hasFoundBall")) {
+	    playLevelScreenState = PlayLevelScreenState.LEVEL_COMPLETED;
+	}
 
-        if (Keyboard.isKeyDown(Key.ESC) && !keyLocker.isKeyLocked(Key.ESC)) {
-        	keyLocker.lockKey(Key.ESC);
-        }
-        else if (Keyboard.isKeyUp(Key.ESC)) {
-            keyLocker.unlockKey(Key.ESC);
-        }
+	if (Keyboard.isKeyDown(Key.ESC) && !keyLocker.isKeyLocked(Key.ESC)) {
+	    keyLocker.lockKey(Key.ESC);
+	} else if (Keyboard.isKeyUp(Key.ESC)) {
+	    keyLocker.unlockKey(Key.ESC);
+	}
+	
+	//////////// Temporary thing, get player location and map name by pressing B ///////////
+	if (Keyboard.isKeyDown(Key.B) && !keyLocker.isKeyLocked(Key.B)) {
+	    System.out.println(player.getLocation());
+	    System.out.println(this.map.getMapFileName());
+	}
+	////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	// map switching, saves state of current map, changes map, places player at certain location
+	if (map.getMapFileName().equals("test_map.txt")) {
+	    if (player.getLocation().x > 160 && player.getLocation().x < 196 && player.getLocation().y == 1260) {
+		springMap = this.map;
+		this.map = interiorMap;
+		this.player.setLocation(374, 408);
+		map.setFlagManager(flagManager);
+		this.player.setMap(map);
+	    }
+	}
+
+	if (map.getMapFileName().equals("interior_map.txt")) {
+	    if (player.getLocation().x > 364 && player.getLocation().x < 380 && player.getLocation().y > 423) {
+		interiorMap = this.map;
+		this.map = springMap;
+		this.player.setLocation(173, 1264);
+		map.setFlagManager(flagManager);
+		this.player.setMap(map);
+	    }
+	}
     }
 
     public void draw(GraphicsHandler graphicsHandler) {
-        // based on screen state, draw appropriate graphics
-        switch (playLevelScreenState) {
-            case RUNNING:
-                map.draw(player, graphicsHandler);
-                break;
-            case LEVEL_COMPLETED:
-                winScreen.draw(graphicsHandler);
-                break;
-        }
+	// based on screen state, draw appropriate graphics
+	switch (playLevelScreenState) {
+	case RUNNING:
+	    map.draw(player, graphicsHandler);
+	    break;
+	case LEVEL_COMPLETED:
+	    winScreen.draw(graphicsHandler);
+	    break;
+	}
     }
 
     public PlayLevelScreenState getPlayLevelScreenState() {
-        return playLevelScreenState;
+	return playLevelScreenState;
     }
 
-
     public void resetLevel() {
-        initialize();
+	initialize();
     }
 
     public void goBackToMenu() {
-        screenCoordinator.setGameState(GameState.MENU);
+	screenCoordinator.setGameState(GameState.MENU);
     }
 
     // This enum represents the different states this screen can be in
     private enum PlayLevelScreenState {
-        RUNNING, LEVEL_COMPLETED
+	RUNNING, LEVEL_COMPLETED
     }
-    
+
     public void playMusic(int i) {
-		sound.setFile(i);
-		sound.play();
-		sound.loop();
-	}
-	
-	public  void stopMusic() {
-		sound.stop();
-	}
-	
-	
+	sound.setFile(i);
+	sound.play();
+	sound.loop();
+    }
+
+    public void stopMusic() {
+	sound.stop();
+    }
+
 }
