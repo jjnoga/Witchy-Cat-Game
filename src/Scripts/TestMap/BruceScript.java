967c767823e90a53af498a5a53f0e4d626626cf8
cakeller2 <cakeller2@quinnipiac.edu>
2022-11-08 22:58:32 -0500

    Bruce Script finished

--- src/Scripts/TestMap/BruceScript.java
+++ src/Scripts/TestMap/BruceScript.java
@@ -34,7 +34,7 @@ public class BruceScript extends Script<NPC> {
 		// changes what Bruce says when talking to him the first time (flag is not set)
 		// vs talking to him afterwards (flag is set)
 		if (!isFlagSet("hasTalkedtoBruce")) {
-			for (int i = 0; i <= 15; i++) {
+			for (int i = 0; i <= 17; i++) {
 				String str = fileInput.nextLine();
 				String multiLine = "\\n";
 				String select = "*ans";
@@ -53,16 +53,41 @@ public class BruceScript extends Script<NPC> {
 				}
 
 			}
-			
-			setFlag("hasTalkedtoBruce");
 
-		} else {
-			for (int i = 0; i <= 34; i++) {
+			setFlag("hasTalkedtoBruce");
+		} else if (isFlagSet("hasTalkedtoBruce") && !isFlagSet("hasTalkedtoBruceTwo")) {
+			for (int i = 0; i <= 40; i++) {
 				fileInput.nextLine();
 			}
 			
-			addTextToTextboxQueue(fileInput.nextLine());
-			System.out.println(IntroScript.firstPick);
+			addTextToTextboxQueue(fileInput.nextLine(), selections, answers);
+			setFlag("hasTalkedtoBruceTwo");
+		} else {
+			for (int i = 0; i <= 42; i++) {
+				fileInput.nextLine();
+			}
+			numAnswer = -1;
+			
+			for (int i = 0; i <= 1; i++) {
+				String str = fileInput.nextLine();
+				String multiLine = "\\n";
+				String select = "*ans";
+				Boolean isMulti = str.contains(multiLine);
+				Boolean isAns = str.contains(select);
+				if (isAns == true && isMulti == true) {
+					numAnswer += 1;
+					answers[numAnswer] = fileInput.nextLine() + "\n" + fileInput.nextLine();
+				} else if (isAns == true && isMulti == false) {
+					numAnswer += 1;
+					answers[numAnswer] = fileInput.nextLine();
+				} else if (isMulti == true) {
+					addTextToTextboxQueue(fileInput.nextLine() + "\n" + fileInput.nextLine());
+				} else if (isMulti == false) {
+					addTextToTextboxQueue(str);
+				}
+
+			}
+			addTextToTextboxQueue(fileInput.nextLine(), selections, answers);
 		}
 		entity.facePlayer(player);
 
